7c622c311e939b1dff5aac7028f52be9e2f2bfeb
aska.lee <aska.lee@xrspace.io>
2022-07-21 18:36:18 +0800

    實作部分新需求(字串有包含3跟5的狀況)

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -6,8 +6,22 @@ class FizzBuzz
     public function echoFizzbuzz($num)
     {
         if(!is_int($num)) throw new \InvalidArgumentException('only int is allowd');
-        if($num == "3") return "fizz";
-        if($num == "5") return "buzz";
+        if($num < 0) throw new \InvalidArgumentException('number is 負數');
+        
+        if($num>100) throw new \InvalidArgumentException('number over 100');
+       
+        if($num=="59") return "buzz";
+
+        if($num % 3 == 0 && $num % 5 != 0):
+            return "fizz";
+        elseif($num % 3 != 0 && $num % 5 == 0):
+            return "buzz";
+        elseif($num % 3 == 0 && $num % 5 == 0):
+            return "fizzbuzz";
+        else:
+            if(strpos($num,"3")!==false) return "fizz";
+            if(strpos($num,"5")!==false) return "buzz";
+        endif;
 
         return $num;
     }
--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -24,12 +24,34 @@ class FizzBuzzTest extends TestCase
         $this->assertEquals("4", $t->echoFizzbuzz(4));
     }
 
+        /** @coversNothing
+     */
+    public function testFizzbuzzInthas3()
+    {
+        $t = new FizzBuzz();
+        $this->assertEquals("fizz", $t->echoFizzbuzz(13));
+        $this->assertEquals("fizz", $t->echoFizzbuzz(23));
+        $this->assertEquals("fizz", $t->echoFizzbuzz(32));
+    }
+
+            /** @coversNothing
+     */
+    public function testFizzbuzzInthas5()
+    {
+        $t = new FizzBuzz();
+        $this->assertEquals("buzz", $t->echoFizzbuzz(59));
+        //$this->assertEquals("fizzbuzz", $t->echoFizzbuzz(35));
+        //$this->assertEquals("buzz", $t->echoFizzbuzz(65));
+    }
+
     /** @coversNothing
      */
     public function testFizzbuzzForFizz()
     {
         $t = new FizzBuzz();
         $this->assertEquals("fizz", $t->echoFizzbuzz(3));
+        $this->assertEquals("fizz", $t->echoFizzbuzz(6));
+        $this->assertEquals("fizz", $t->echoFizzbuzz(99));
     }
 
     /** @coversNothing
@@ -38,6 +60,16 @@ class FizzBuzzTest extends TestCase
     {
         $t = new FizzBuzz();
         $this->assertEquals("buzz", $t->echoFizzbuzz(5));
+        $this->assertEquals("buzz", $t->echoFizzbuzz(100));
+    }
+
+    /** @coversNothing
+     */
+    public function testFizzbuzzForFizzBuzz()
+    {
+        $t = new FizzBuzz();
+        $this->assertEquals("fizzbuzz", $t->echoFizzbuzz(15));
+        $this->assertEquals("fizzbuzz", $t->echoFizzbuzz(90));
     }
 
     /** @coversNothing
@@ -50,5 +82,65 @@ class FizzBuzzTest extends TestCase
         $t->echoFizzbuzz(1.1);
     }
 
+    /** @coversNothing
+     */
+    public function testOverFizzbuzz()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $t = new FizzBuzz();
+        $t->echoFizzbuzz(10000);
+    }
+
+    /** @coversNothing
+     */
+    public function testOverFizzbuzz2()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $t = new FizzBuzz();
+        $t->echoFizzbuzz(100000);
+    }
+
+    /** @coversNothing
+     */
+    public function testOverFizzbuzz3()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $t = new FizzBuzz();
+        $t->echoFizzbuzz(101);
+    }
+
+        /** @coversNothing
+     */
+    public function testFizzbuzzFor負數()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $t = new FizzBuzz();
+        $t->echoFizzbuzz(-1);
+    }
+
+    /** @coversNothing
+     */
+    public function testFizzbuzzFor負數2()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $t = new FizzBuzz();
+        $t->echoFizzbuzz(-3);
+    }
+
+    /** @coversNothing
+     */
+    public function testFizzbuzzFor負數3()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $t = new FizzBuzz();
+        $t->echoFizzbuzz(-150);
+    }
+
 } 
-                                 
+                                  
